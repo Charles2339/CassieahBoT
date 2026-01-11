3c7179627b7cd604fc5e02375ea4eeb3d6911b1d
lianecagara <eckobautista03@gmail.com>
2026-01-11 22:51:17 +0800

    improved Deferred.ts

--- CommandFiles/modules/Deferred.ts
+++ CommandFiles/modules/Deferred.ts
@@ -7,15 +7,22 @@ export class Deferred<Res, Rej = unknown> extends Promise<Res> {
       rej = rej_cb;
     });
     this.resolve = (value) => {
+      if (this.status !== "pending") return;
       this.value = value;
+      this.status = "fulfilled";
       res(value);
     };
     this.reject = (reason) => {
+      if (this.status !== "pending") return;
       this.rejectReason = reason;
+      this.status = "rejected";
       rej(reason);
     };
     this.value = initialValue ?? null;
+    this.status = "pending";
   }
+
+  status: "pending" | "fulfilled" | "rejected";
 
   value: Res | null;
 
