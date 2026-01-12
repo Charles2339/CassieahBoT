d88c738854438a9b14a6462b2af545f3124bca92
lianecagara <eckobautista03@gmail.com>
2026-01-12 19:52:52 +0800

    KayeBotEvent tweaks

--- CommandFiles/modules/KayeEventWrapper.ts
+++ CommandFiles/modules/KayeEventWrapper.ts
@@ -1,30 +1,150 @@
 import EventEmitter from "events";
 import Stream from "stream";
 
-export class KayeBotEvent {
-  implEvents: EventEmitter<KayeBotEvent.ImplementationEvents>;
+export class KayeBotEvent implements KayeBotEvent.RawEvent {
+  implHandlers: EventEmitter<KayeBotEvent.ImplementationEvents>;
 
-  constructor() {
-    this.implEvents = new EventEmitter();
+  args: KayeBotEvent.RawEvent["args"];
+  body: KayeBotEvent.RawEvent["body"];
+  messageID: KayeBotEvent.RawEvent["messageID"];
+  reaction: KayeBotEvent.RawEvent["reaction"];
+  senderID: KayeBotEvent.RawEvent["senderID"];
+  threadID: KayeBotEvent.RawEvent["threadID"];
+  type: KayeBotEvent.RawEvent["type"];
+
+  constructor(
+    data: Partial<KayeBotEvent.RawEvent>,
+    handlers?: KayeBotEvent["implHandlers"]
+  ) {
+    this.implHandlers = handlers ?? new EventEmitter();
+
+    this.body = data.body ?? "";
+    this.messageID = data.messageID ?? "";
+    this.reaction = data.reaction ?? "";
+    this.type = data.type ?? "message";
+    this.threadID = data.threadID ?? "";
+    this.senderID = data.senderID ??= "";
+    this.args = data.args ??= this.body.split(" ").filter(Boolean);
   }
 
-  dispatch(form: KayeBotEvent.DispatchForm): KayeBotEvent.Response;
+  dispatch(form: KayeBotEvent.DispatchForm): KayeBotEvent.Dispatched;
   dispatch(
     body: string,
-    form: Exclude<KayeBotEvent.DispatchFormStrict, "body">
-  ): KayeBotEvent.Response;
+    form: KayeBotEvent.DispatchFormNoBody
+  ): KayeBotEvent.Dispatched;
 
-  dispatch(): KayeBotEvent.Response {}
+  dispatch(
+    formOrBody: KayeBotEvent.DispatchForm,
+    form2?: KayeBotEvent.DispatchFormNoBody
+  ): KayeBotEvent.Dispatched {
+    const form: KayeBotEvent.DispatchFormStrict =
+      typeof formOrBody === "string"
+        ? {
+            body: formOrBody,
+            ...(typeof form2 === "object" && form2 ? form2 : {}),
+          }
+        : { ...formOrBody };
+    const result = new KayeBotEvent.Dispatched();
+    let finalBody = form.body;
+    form.bodyMode ??= "random";
+    if (Array.isArray(finalBody)) {
+      if (form.bodyMode === "random") {
+        finalBody = KayeBotEvent.randArr(finalBody);
+      } else if (form.bodyMode === "line-break") {
+        finalBody = finalBody.join("\n");
+      } else {
+        finalBody = "[Invalid Body Mode]";
+      }
+    }
+    const finalForm: KayeBotEvent.SuppliedDispatchForm = {
+      ...form,
+      finalBody,
+      thread: form.thread ?? this.threadID,
+      replyTo: form.replyTo ?? this.messageID,
+    };
+    if (this.hasAnyHandler("dsptchFull") && !form.forceAsText) {
+      this.implHandlers.emit("dsptchFull", finalForm, result);
+    } else if (this.hasAnyHandler("dsptchTxt")) {
+      this.implHandlers.emit("dsptchTxt", finalForm, result);
+    } else {
+      throw new KayeBotEvent.KayeBotErr(
+        `No handlers set for dsptchFull and dsptchTxt.`
+      );
+    }
+    return result;
+  }
 
-  reply() {}
+  reply(
+    form: KayeBotEvent.DispatchForm,
+    replyTo = this.messageID,
+    thread = this.threadID
+  ): KayeBotEvent.Dispatched {
+    const normal = KayeBotEvent.normalizeForm(form);
+    const result = this.dispatch({
+      ...normal,
+      replyTo: replyTo ?? normal.replyTo,
+      thread: thread ?? normal.thread,
+    });
+    return result;
+  }
+
+  send(
+    form: Omit<KayeBotEvent.DispatchForm, "replyTo">,
+    thread = this.threadID
+  ): KayeBotEvent.Dispatched {
+    const normal = KayeBotEvent.normalizeForm(form);
+    const result = this.dispatch({
+      ...normal,
+      replyTo: null,
+      thread: thread ?? normal.thread,
+    });
+    return result;
+  }
+
+  getOneHandler<K extends keyof KayeBotEvent.ImplementationEvents>(key: K) {
+    const list = this.implHandlers.listeners(key);
+    return KayeBotEvent.randArr(list);
+  }
+
+  getHandlerCount<K extends keyof KayeBotEvent.ImplementationEvents>(
+    key: K
+  ): number {
+    return this.implHandlers.listenerCount(key);
+  }
+
+  hasAnyHandler<K extends keyof KayeBotEvent.ImplementationEvents>(
+    key: K
+  ): boolean {
+    return this.getHandlerCount(key) >= 1;
+  }
 }
 
 export namespace KayeBotEvent {
-  export interface BotEvents {
-    reply: [KayeBotEvent];
-    reaction: [KayeBotEvent];
+  export function randArr<T>(arr: T[]): T | undefined {
+    return arr.at(Math.floor(Math.random() * arr.length));
   }
-  export interface ImplementationEvents {}
+  export interface BotEvents {
+    reply: [KayeBotEvent, string];
+    reaction: [KayeBotEvent, string];
+    listen_replies: [];
+    listen_reactions: [];
+    stop_listen_replies: [];
+    stop_listen_reactions: [];
+  }
+  export interface ImplementationEvents {
+    dsptchTxt: [SuppliedDispatchForm, Dispatched];
+    dsptchFull: [SuppliedDispatchForm, Dispatched];
+  }
+
+  export interface RawEvent {
+    body: string;
+    senderID: string;
+    threadID: string;
+    messageID: string;
+    args: string[];
+    reaction: string;
+    type: "message" | "message_reply" | "message_reaction";
+  }
 
   export type DispatchForm = string | DispatchFormStrict;
 
@@ -32,28 +152,51 @@ export namespace KayeBotEvent {
     body?: string | string[];
     bodyMode?: "random" | "line-break";
     attachments?: Stream | Stream[];
+    forceAsText?: boolean;
+    destination?: string;
+    thread?: string;
+    replyTo?: string;
   }
 
-  export class Response
+  export interface SuppliedDispatchForm extends DispatchFormStrict {
+    finalBody: string;
+    thread: string;
+    replyTo: string;
+  }
+
+  export type DispatchFormNoBody = Omit<DispatchFormStrict, "body">;
+
+  export class Dispatched
     extends EventEmitter<BotEvents>
-    implements PromiseLike<Response>
+    implements PromiseLike<Dispatched>
   {
-    promiseInternal: Promise<Response>;
+    private promiseInternal: Promise<Dispatched>;
+
     constructor() {
       super();
-      let res: Response["resolveInternal"], rej: Response["rejectInternal"];
-      const promise = new Promise<Response>((resolve, reject) => {
+      let res: Dispatched["resolveInternal"], rej: Dispatched["rejectInternal"];
+      const promise = new Promise<Dispatched>((resolve, reject) => {
         res = resolve;
         rej = reject;
       });
       this.promiseInternal = promise;
       this.resolveInternal = res;
       this.rejectInternal = rej;
+      this.#ready = false;
+      this.error = null;
     }
 
-    then<TResult1 = Response, TResult2 = never>(
+    error: null | any;
+
+    #ready: boolean;
+
+    isReady() {
+      return this.#ready;
+    }
+
+    then<TResult1 = Dispatched, TResult2 = never>(
       onfulfilled?:
-        | ((value: Response) => TResult1 | PromiseLike<TResult1>)
+        | ((value: Dispatched) => TResult1 | PromiseLike<TResult1>)
         | undefined
         | null,
       onrejected?:
@@ -64,7 +207,67 @@ export namespace KayeBotEvent {
       return this.promiseInternal.then(onfulfilled, onrejected);
     }
 
-    resolveInternal(_value: Response | PromiseLike<Response>) {}
-    rejectInternal(_reason?: any) {}
+    private resolveInternal(_value: Dispatched | PromiseLike<Dispatched>) {}
+    private rejectInternal(_reason?: any) {}
+
+    resolveResponse(info: DispatchedInfo | null, err?: any) {
+      if (this.isReady()) {
+        throw new KayeBotErr("Already resolved.");
+      }
+      if (err) {
+        this.error = err;
+        this.rejectInternal(err);
+        return;
+      }
+      this.#ready = true;
+      this.messageID = info.messageID;
+      this.timestamp = info.timestamp;
+      this.threadID = info.threadID;
+      this.resolveInternal(this);
+    }
+
+    messageID: DispatchedInfo["messageID"];
+    timestamp: DispatchedInfo["timestamp"];
+    threadID: DispatchedInfo["threadID"];
+
+    listenReplies() {
+      this.emit("listen_replies");
+    }
+
+    stopListenReplies() {
+      this.emit("stop_listen_replies");
+    }
+
+    listenReactions() {
+      this.emit("listen_reactions");
+    }
+
+    stopListenReactions() {
+      this.emit("stop_listen_reactions");
+    }
+  }
+
+  export interface DispatchedInfo {
+    messageID: string;
+    timestamp: number;
+    threadID: string;
+  }
+
+  export class KayeBotErr extends Error {
+    constructor(message?: string, options?: ErrorOptions) {
+      super(message, options);
+      this.name = KayeBotErr.name;
+    }
+
+    toString() {
+      return this.stack ?? this.message;
+    }
+  }
+
+  export function normalizeForm(form: DispatchForm): DispatchFormStrict {
+    if (typeof form === "string") {
+      return { body: form };
+    }
+    return { ...form };
   }
 }
